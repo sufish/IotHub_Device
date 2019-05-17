966146bb84896497bfb440b7de5518d6c2577244
fuqiang <fuqiang@didikon.com>
2019-05-17 18:29:13 +0800

    finish 1-2

--- test_mqtt.js
+++ test_mqtt.js
@@ -1,6 +1,9 @@
 //
 var mqtt = require('mqtt')
-var client = mqtt.connect('mqtt://127.0.0.1:1883')
+var client = mqtt.connect('mqtt://127.0.0.1:1883', {
+    username: "test",
+    password: "123456"
+})
 client.on('connect', function (connack) {
     console.log(`return code: ${connack.returnCode}`)
     client.end()
