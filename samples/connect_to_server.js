93f604e09e6d83cce3a179f3deb94cfb0b618d3d
fuqiang <fuqiang@didikon.com>
2019-05-20 14:50:25 +0800

    finish 1-3

--- samples/connect_to_server.js
+++ samples/connect_to_server.js
@@ -1,6 +1,11 @@
 var IotDevice = require("../sdk/iot_device")
+require('dotenv').config()
 
-var device = new IotDevice({productName: "IotApp", deviceName: "V5MyuncRK", secret: "GNxU20VYTZ"})
+var device = new IotDevice({
+    productName: process.env.PRODUCT_NAME,
+    deviceName: process.env.DEVICE_NAME,
+    secret: process.env.SECRET
+})
 device.on("online", function () {
     console.log("device is online")
     device.disconnect()
--- sdk/iot_device.js
+++ sdk/iot_device.js
@@ -4,20 +4,27 @@ var mqtt = require('mqtt')
 const EventEmitter = require('events');
 
 class IotDevice extends EventEmitter {
-    constructor({serverAddress = "127.0.0.1:8883", productName, deviceName, secret} = {}) {
+    constructor({serverAddress = "127.0.0.1:8883", productName, deviceName, secret, clientID} = {}) {
         super();
         this.serverAddress = `mqtts://${serverAddress}`
         this.productName = productName
         this.deviceName = deviceName
         this.secret = secret
         this.username = `${this.deviceName}@${this.productName}`
+        if(clientID != null){
+            this.clientIdentifier = `${clientID}@${this.deviceName}@${this.productName}`
+        }else{
+            this.clientIdentifier = `${this.deviceName}@${this.productName}`
+        }
     }
 
     connect() {
         this.client = mqtt.connect(this.serverAddress, {
             rejectUnauthorized: false,
             username: this.username,
-            password: this.secret
+            password: this.secret,
+            clientId: this.clientIdentifier,
+            clean: false
         })
         var self = this
         this.client.on("connect", function () {
