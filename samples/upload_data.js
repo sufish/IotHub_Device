5b79be1444e741ab4b9c5b0bdfd612ee3378258c
fuqiang <fuqiang@didikon.com>
2019-05-27 15:17:04 +0800

    2-4

--- samples/upload_data.js
+++ samples/upload_data.js
@@ -1,11 +1,12 @@
 var IotDevice = require("../sdk/iot_device")
 require('dotenv').config()
-
+var path = require('path');
 var device = new IotDevice({
     productName: process.env.PRODUCT_NAME,
     deviceName: process.env.DEVICE_NAME,
     secret: process.env.SECRET,
-    clientID: "upload_data.js"
+    clientID: path.basename(__filename, ".js"),
+    storePath: `../tmp/${path.basename(__filename, ".js")}`
 })
 device.on("online", function () {
     console.log("device is online")
--- sdk/iot_device.js
+++ sdk/iot_device.js
@@ -1,11 +1,12 @@
 //iot_device.js
 "use strict";
+var levelStore = require('mqtt-level-store');
 const ObjectId = require('bson').ObjectID;
 var mqtt = require('mqtt')
 const EventEmitter = require('events');
 
 class IotDevice extends EventEmitter {
-    constructor({serverAddress = "127.0.0.1:8883", productName, deviceName, secret, clientID} = {}) {
+    constructor({serverAddress = "127.0.0.1:8883", productName, deviceName, secret, clientID, storePath} = {}) {
         super();
         this.serverAddress = `mqtts://${serverAddress}`
         this.productName = productName
@@ -17,16 +18,24 @@ class IotDevice extends EventEmitter {
         }else{
             this.clientIdentifier = this.username
         }
+        if(storePath != null) {
+            this.manager = levelStore(storePath);
+        }
     }
 
     connect() {
-        this.client = mqtt.connect(this.serverAddress, {
+        var opts = {
             rejectUnauthorized: false,
             username: this.username,
             password: this.secret,
             clientId: this.clientIdentifier,
             clean: false
-        })
+        };
+        if(this.manager != null){
+            opts.incomingStore = this.manager.incoming
+            opts.outgoingStore = this.manager.outgoing
+        }
+        this.client = mqtt.connect(this.serverAddress, opts)
         var self = this
         this.client.on("connect", function () {
             self.emit("online")
